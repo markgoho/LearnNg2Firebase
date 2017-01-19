7cd34a6a95d2af2d13fa08a57952a0a29f495084
Mark Goho <markgoho@gmail.com>
2017-01-18 21:36:11 -0500

    Section2 Lecture8 SDK setup in App.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { initializeApp, database } from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor() {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyApm-N_4fD_KRqme0XMnYJOmunHV1W3jJ4",
+      authDomain: "finalproject-7b759.firebaseapp.com",
+      databaseURL: "https://finalproject-7b759.firebaseio.com",
+      storageBucket: "finalproject-7b759.appspot.com",
+      messagingSenderId: "459964997051"
+    };
+    initializeApp(config);
+
+    const root = database().ref('messages/2');
+    root.on('value', snap => {
+      console.log(snap.key, snap.val());
+    })
+  }
 }
