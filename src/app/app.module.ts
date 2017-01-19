7f6bb24af7d8c8d446b855d021dfef216754b5de
Mark Goho <markgoho@gmail.com>
2017-01-19 17:07:12 -0500

    Section4 Lecture25 Routing config setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
+import {RouterModule} from '@angular/router';
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
-
 
 import {AppComponent} from './app.component';
 import {firebaseConfig} from '../environments/firebase.config';
 import { HomeComponent } from './home/home.component';
 import { LessonsService } from './shared/model/lessons.service';
 import { LessonsListComponent } from './lessons-list/lessons-list.component';
+import {routerConfig} from './router.config';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { LessonsListComponent } from './lessons-list/lessons-list.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AngularFireModule.initializeApp(firebaseConfig)
+    AngularFireModule.initializeApp(firebaseConfig),
+    RouterModule.forRoot(routerConfig)
   ],
   providers: [LessonsService],
   bootstrap: [AppComponent]
